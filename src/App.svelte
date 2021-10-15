70b15871eb9208b0a94f12e187eb73b42f0b3d61
Philip Huang <philip.sw.huang@gmail.com>
2021-10-14 19:13:12 -0700

    Initial Svelte syntax, begin branches

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+	let name = "Phil";
+	let age = 32;
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function resetAge() {
+		age = 32;
+	}
 </script>
+
+<h1>Hello {name}! My age is {age}.</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={resetAge}>Reset Age</button>
 
 <style>
 	h1 {
 		color: purple;
 	}
 </style>
-
-<h1>Hello {name}!</h1>
