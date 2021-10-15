deb3d370ce91101ef1e8bbb9ece3645929ea772c
Philip Huang <philip.sw.huang@gmail.com>
2021-10-14 21:27:30 -0700

    dynamic css/exporting lets/functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,60 @@
 <script>
+	import ContactCard from "./ContactCard.svelte";
+
 	let name = "Phil";
 	let age = 32;
+	let jobTitle = "Software Engineer";
+	let description = "lorem ipsum";
+	let url = "";
+
+	$: upperCaseName = name.toUpperCase();
+
+	$: if (name === "Philip") {
+		age = 23;
+	}
 
 	function incrementAge() {
 		age += 1;
 	}
 
+	function changeName() {
+		name = "Philip";
+	}
+
 	function resetAge() {
 		age = 32;
 	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
 </script>
 
-<h1>Hello {name}! My age is {age}.</h1>
-<button on:click={incrementAge}>Change Age</button>
+<h1>Hello {upperCaseName}! My age is {age}.</h1>
+<button on:click={incrementAge}>Add Age</button>
 <button on:click={resetAge}>Reset Age</button>
+<button on:click={changeName}>Change Name</button>
+
+<div>
+	Name: <input type="text" bind:value={name} />
+</div>
+
+<div>
+	Job Title: <input type="text" bind:value={jobTitle} />
+</div>
+
+<div>
+	Description: <textarea bind:value={description} cols="50" rows="3" />
+</div>
+
+<div>
+	Img Src: <input type="text" bind:value={url} />
+</div>
+
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+
+<ContactCard {name} {jobTitle} {description} {url} />
 
 <style>
 	h1 {
---
+++ src/Assignment.svelte
@@ -0,0 +1,26 @@
+<script>
+  let courseGoal = "Course Goals";
+  let color = "";
+
+  function exclamation() {
+    let reg = /[!]/;
+    if (reg.test(courseGoal)) {
+      console.log("woo!");
+      color = "color: red";
+      console.log(color);
+    } else {
+      color = "";
+    }
+  }
+</script>
+
+<div>
+  <input type="text" bind:value={courseGoal} on:input={exclamation} />
+
+  <h1 style={color}>
+    {courseGoal}
+  </h1>
+</div>
+
+<style>
+</style>
